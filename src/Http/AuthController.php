855cc57fc6a2a1b68e4942dfa9b1a3ee1efcd389
Grant Brits <gbrits@hotmail.com>
2019-02-13 18:08:39 +0800

    Update AuthController.php

--- src/Http/AuthController.php
+++ src/Http/AuthController.php
@@ -4,71 +4,30 @@ namespace Gbrits\Firebase\Auth\Http;
 
 use Gbrits\Firebase\Auth\User;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\Auth\RegistersUsers;
 use Gbrits\Firebase\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\RegistersUsers;
 
 class AuthController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login / registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
-
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
-
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'id_token' => 'required',
-            'remember' => 'boolean',
-            'email' => 'email',
-            'photo_url' => 'url',
-        ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'firebase_uid' => $data['uid'],
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'photo_url' => $data['photo_url'],
-        ]);
-    }
-
+  use RegistersUsers, AuthenticatesUsers;
+  protected $redirectTo = '/';
+  public function __construct()
+  {
+    $this->middleware('guest');
+  }
+  protected function validator(array $data)
+  {
+    return Validator::make($data, [
+      'name'          => 'required|max:255',
+      'id_token'      => 'required'
+    ]);
+  }
+  protected function create(array $data)
+  {
+    $user = User::create([
+      'name' => $data['name'],
+      'id_token' => $data['id_token']
+    ]);
+    return $user;
+  }
 }
