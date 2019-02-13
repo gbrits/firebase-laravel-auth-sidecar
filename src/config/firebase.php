27a9c3bb5a1bc6adfdafb02252d9412f1921faf6
Grant Brits <gbrits@hotmail.com>
2019-02-13 10:58:45 +0800

    Update config array

--- src/config/firebase.php
+++ src/config/firebase.php
@@ -1,11 +1,9 @@
 <?php
-
 return [
-
-  'project_id' => env('FIREBASE_PROJECT_ID', null),
-
   'api_key' => env('FIREBASE_API_KEY', null),
-
   'auth_domain' => env('FIREBASE_AUTH_DOMAIN', null),
-
+  'database_url' => env('FIREBASE_DATABASE_URL', null),
+  'project_id' => env('FIREBASE_PROJECT_ID', null),
+  'storage_bucket' => env('FIREBASE_STORAGE_BUCKET', null),
+  'messaging_sender_id' => env('FIREBASE_MESSAGING_SENDER_ID', null)
 ];
